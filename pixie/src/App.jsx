b137db449665f1c724be3ea8b91f3c87108094a8
Brian Joo <brianhjoo@gmail.com>
2023-05-27 23:54:57 -0700

    added useEffect to fetch countries on mount

--- pixie/src/App.jsx
+++ pixie/src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
+import PixieAPI from "./api";
+
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [errors, setErrors] = useState(null);
+
+  useEffect(function fetchCountriesOnMount() {
+    async function getCountries() {
+      try {
+        const countries = await PixieAPI.getCountries();
+        setCountries(countries);
+      } catch(err) {
+          setErrors(err);
+      }
+    }
+    getCountries();
+  }, []);
+
+  if (errors) return 'Something went wrong!';
+
   return (
-    <>
-     <h1>Pixie</h1>
-    </>
-  )
+    <ul className="m-6">
+      {countries.map((country) => (
+        <li key={country.name} className="text-blue-500">{country.name}</li>
+      ))}
+    </ul>
+  );
 }
 
-export default App
+export default App;
