2477ae7ea5817d4f311bca981956d64d49e3e654
Brian Joo <brianhjoo@gmail.com>
2023-05-29 00:01:42 -0700

    set up skeleton app file

--- pixie-backend/app.py
+++ pixie-backend/app.py
@@ -1,18 +1,22 @@
+import os
+
 from flask import Flask, request, redirect, render_template, flash, jsonify
 from werkzeug.exceptions import Unauthorized
 from flask_debugtoolbar import DebugToolbarExtension
-import os
+from dotenv import load_dotenv
+
+from models import db, connect_db, User
+
+load_dotenv()
 
 app = Flask(__name__)
 
-from models import db, connect_db
-
-connect_db(app)
-
-app.config['SECRET_KEY'] = 'this_is_secret'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 
 debug = DebugToolbarExtension(app)
+
+connect_db(app)
