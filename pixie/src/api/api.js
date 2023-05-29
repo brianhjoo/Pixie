e68adb11de7caa7ed2433abf93dd2511c9f639be
Brian Joo <brianhjoo@gmail.com>
2023-05-28 17:38:30 -0700

    deleted test class method

--- pixie/src/api/api.js
+++ pixie/src/api/api.js
@@ -2,15 +2,12 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_PROJECT_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 class PixieAPI {
-  /** Get all countries */
-  static async getCountries() {
-    const { data } = await supabase.from("countries").select();
-    return data;
-  }
+
 }
 
 export default PixieAPI;
