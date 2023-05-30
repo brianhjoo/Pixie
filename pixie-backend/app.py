79a3526592e70ffebd854f7cd2efab8c4aa12d3e
Brian Joo <brianhjoo@gmail.com>
2023-05-29 23:09:58 -0700

    created skeleton signup route that takes user data and returns a JWT in json format

--- pixie-backend/app.py
+++ pixie-backend/app.py
@@ -6,6 +6,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from dotenv import load_dotenv
 
 from models import db, connect_db, User
+from helpers.token import create_token
 
 load_dotenv()
 
@@ -20,3 +21,21 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+
+#============================================================== User Auth ====#
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    ''' Handle user signup. '''
+
+    user_data = request.json
+
+    # TODO: Need to send user_data to models for processing.
+
+    token = create_token({
+        'username': user_data['username'],
+        'is_admin': user_data['is_admin'],
+    })
+
+    return jsonify(token)
