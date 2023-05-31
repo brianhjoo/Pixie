63ce9022adda11c7164545b68c12b6d658fdc9e2
Brian Joo <brianhjoo@gmail.com>
2023-05-30 23:16:47 -0700

    fixed error handling and passing in user that just signed up to create_token

--- pixie-backend/app.py
+++ pixie-backend/app.py
@@ -30,25 +30,30 @@ connect_db(app)
 def signup():
     ''' Handle user signup. '''
 
-    user_data = request.json
+    user_data = request.get_json()
+
+    user = User.signup(
+        username=user_data['username'],
+        password=user_data['password'],
+        first_name=user_data['first_name'],
+        last_name=user_data['last_name'],
+        email=user_data['email'],
+        is_admin=False,
+    )
 
     try:
-        user = User.signup(
-            username=user_data['username'],
-            password=user_data['password'],
-            first_name=user_data['first_name'],
-            last_name=user_data['last_name'],
-            email=user_data['email'],
-            is_admin='false',
-        )
         db.session.commit()
     except IntegrityError:
         db.session.rollback()
-        return IntegrityError
+        return jsonify({
+            'message': 'Something went wrong!',
+            'code': 'integrity_error',
+            'status_code': 500,
+        })
 
     token = create_token({
-        'username': user['username'],
-        'is_admin': user['is_admin'],
+        'username': user.username,
+        'is_admin': user.is_admin,
     })
 
     return jsonify(token=token)
